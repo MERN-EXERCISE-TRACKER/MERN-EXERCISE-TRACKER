8519143b3fd5389db3c9f5ffed44bc30c4472c1b
emanabuwaked <emanabuwaked@gmail.com>
2020-10-19 22:22:00 +0300

    exercise

--- exercise-tracker/backend/routers/users.js
+++ exercise-tracker/backend/routers/users.js
@@ -1,5 +1,20 @@
 const express = require("express")
-const router = require("express") .Router()
+const router = express.Router()
 const User = require ("../models/user.model")
 
-router   
+router.route("/").get((req, res)=>{
+    User
+    .find()
+    .then(usres=>res.json(users))
+    .catch(error => res.status(400).json("Error: "+error))
+}) 
+
+router.route("/add").post((req,res)=>{
+    const username = req.body.username;
+    const newUser = new User ({username})
+
+    newUser.save()
+    .then(()=> res.json("User added!"))
+    .catch((error)=> res.status(400).json("Error: "+error))
+})
+//////////
